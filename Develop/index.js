77a29f8a73c972df147dc0e79820512e5221264b
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-07-25 15:48:33 -0600

    added some questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,33 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
