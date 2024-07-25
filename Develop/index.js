aea6b69b3dbeb090cacf7547c6382b6a4efd5e2a
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-07-25 15:58:35 -0600

    added markdown for readme

--- Develop/index.js
+++ Develop/index.js
@@ -52,6 +52,41 @@ const questions = [
       },
 ];
 
+function generateMarkdown(data) {
+    return `
+  # ${data.title}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions, please open an issue or contact [${data.github}](https://github.com/${data.github}) at ${data.email}.
+    `;
+  }
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
